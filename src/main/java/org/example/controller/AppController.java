22582e07b5bfbddb9028bcca0748932d5bec4d89
Yevhen-Pashkulskyi <pashkulskyevhen@gmail.com>
2024-03-19 09:35:48 +0200

    version_02

--- src/main/java/org/example/controller/AppController.java
+++ src/main/java/org/example/controller/AppController.java
@@ -1,5 +1,43 @@
 package org.example.controller;
+import org.example.model.PostModel;
+import org.example.view.MenuView;
+import org.example.view.PostView;
 
 public class AppController {
 
+    private final PostModel model;
+    private final PostView view;
+    private final MenuView menuView;
+    private final PostController postController;
+
+    public AppController(PostModel model, PostView view, MenuView menuView) {
+        this.model = model;
+        this.view = view;
+        this.menuView = menuView;
+        this.postController = new PostController(model, view);
+    }
+
+    public void runApplication() {
+        boolean running = true;
+        while (running) {
+            int choice = menuView.displayMenuAndGetChoice();
+            switch (choice) {
+                case 1:
+                    postController.getAllPosts();
+                    break;
+                case 2:
+                    int id = menuView.getPostId();
+                    postController.getPostById(id);
+                    break;
+                case 0:
+                    running = false;
+                    System.out.println("Exiting the application...");
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Invalid option. Please try again.");
+                    break;
+            }
+        }
+    }
 }
--- src/main/java/org/example/utils/AppStarter.java
+++ src/main/java/org/example/utils/AppStarter.java
@@ -1,5 +1,6 @@
 package org.example.utils;
-import org.example.controller.PostController;
+
+import org.example.controller.AppController;
 import org.example.model.PostModel;
 import org.example.view.MenuView;
 import org.example.view.PostView;
@@ -10,28 +11,8 @@ public class AppStarter {
         PostModel model = new PostModel();
         PostView view = new PostView();
         MenuView menuView = new MenuView();
-        PostController controller = new PostController(model, view);
+        AppController controller = new AppController(model, view, menuView);
 
-        boolean running = true;
-        while (running) {
-            int choice = menuView.displayMenuAndGetChoice();
-            switch (choice) {
-                case 1:
-                    controller.getAllPosts();
-                    break;
-                case 2:
-                    int id = menuView.getPostId();
-                    controller.getPostById(id);
-                    break;
-                case 0:
-                    running = false;
-                    System.out.println("Exiting the application...");
-                    System.exit(0);
-                    break;
-                default:
-                    System.out.println("Invalid option. Please try again.");
-                    break;
-            }
-        }
+        controller.runApplication();
     }
 }
